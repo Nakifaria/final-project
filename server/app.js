264b6549eeca952b82626d8f655f315445defd07
Ivan Rubtsov <rubtsov.ivan97@gmail.com>
2023-06-13 16:56:47 +0300

    project base created

--- server/app.js
+++ server/app.js
@@ -1,26 +1,24 @@
-require('dotenv').config();
-require('@babel/register');
+require("dotenv").config();
+require("@babel/register");
 
-const session = require('express-session');
-const FileStore = require('session-file-store')(session);
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
 
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-const dbCheck = require('./src/middlewares/dbCheck');
+const dbCheck = require("./src/middlewares/dbCheck");
 
 // Require routes
 
-const userRoute = require('./src/routes/user.route');
-const noteRoute = require('./src/routes/note.route');
-const nbRoute = require('./src/routes/nb.route');
+const userRoute = require("./src/routes/user.route");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Cookie
 const sessionConfig = {
-  name: 'authCookie',
+  name: "authCookie",
   store: new FileStore(),
   secret: process.env.SESSION_SECRET,
   resave: false,
@@ -34,21 +32,19 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(
   cors({
-    origin: ['http://localhost:3001'],
+    origin: ["http://localhost:5173"],
     credentials: true,
   })
 );
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(dbCheck);
 
 // Routes
 
-app.use('/user', userRoute);
-app.use('/note', noteRoute);
-app.use('/notebook', nbRoute);
+app.use("/user", userRoute);
 
 app.listen(PORT, () => {
   console.log(`Server started at ${PORT}`);
