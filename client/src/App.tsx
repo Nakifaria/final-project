7a12f447abe1a10864cf87f443dbc60e15f5037c
Egor Krasilnikov <krasilnikov19@gmail.com>
2023-06-15 11:10:41 +0300

    fix routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,24 @@
-
-import { Link, Route, Routes } from "react-router-dom";
-import Configurator from "./components/Configurator/Configurator";
-import { Catalog } from "./components/Catalog/Catalog";
-import { CategoryCatalog } from "./components/CategoryCatalog/CategoryCatalog";
-import { ItemPage } from "./components/ItemPage/ItemPage";
-import { ToastContainer } from "react-toastify";
-import { Home } from "./components/Home/Home";
+import { Link, Route, Routes } from 'react-router-dom';
+import Configurator from './components/Configurator/Configurator';
+import { Catalog } from './components/Catalog/Catalog';
+import { CategoryCatalog } from './components/CategoryCatalog/CategoryCatalog';
+import { ItemPage } from './components/ItemPage/ItemPage';
+import { ToastContainer } from 'react-toastify';
+import { Home } from './components/Home/Home';
 
 function App() {
-  return <>
-  <Routes>
-    <Route path='/catalog' element={<Catalog/>}/>
-    <Route path='/catalog/category' element={<CategoryCatalog/>}/>
-    <Route path='/catalog/category/product' element={<ItemPage/>}/>
-    <Route path="/configurator" element={<Configurator />} />
-    <Route path="" element={<Home />} />
-  </Routes>
-     <ToastContainer />
-  </>;
-
+  return (
+    <>
+      <Routes>
+        <Route path="catalog" element={<Catalog />} />
+        <Route path="category" element={<CategoryCatalog />} />
+        <Route path="product" element={<ItemPage />} />
+        <Route path="configurator" element={<Configurator />} />
+        <Route element={<Home />} />
+      </Routes>
+      <ToastContainer />
+    </>
+  );
 }
 
 export default App;
