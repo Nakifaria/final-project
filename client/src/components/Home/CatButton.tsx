5d50e70dbe8abf3d162a9ab33697fa478c40eb1d
Egor Krasilnikov <krasilnikov19@gmail.com>
2023-06-16 17:54:29 +0300

    main 60%

--- client/src/components/Home/CatButton.tsx
+++ client/src/components/Home/CatButton.tsx
@@ -2,32 +2,19 @@ import { FC } from 'react';
 
 export interface IbuttonProps {
   title: string;
-  autofocus: boolean;
+
   otherCategory: (cat: number) => void;
   catNumber: number;
 }
 
 export const CatButton: FC<IbuttonProps> = ({
   title,
-  autofocus,
   otherCategory,
   catNumber,
 }) => {
   return (
-    <>
-      {autofocus ? (
-        <button
-          className="catBtn"
-          autoFocus
-          onClick={() => otherCategory(catNumber)}
-        >
-          {title}
-        </button>
-      ) : (
-        <button className="catBtn" onClick={() => otherCategory(catNumber)}>
-          {title}
-        </button>
-      )}
-    </>
+    <button className="catBtn" onClick={() => otherCategory(catNumber)}>
+      {title}
+    </button>
   );
 };
--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -6,60 +6,24 @@ import { CatButton } from './CatButton';
 import { ItemCard } from './itemCard';
 import { useEffect, useState } from 'react';
 import { IItem } from '../../redux/slices/items.slice';
+import { FilteredItemsBox } from './FilteredItemsBox';
 
 export const Home = () => {
-  const aboutItems = useSelector((state: RootState) => state.itemsSlice.items);
-
-  const allItems = aboutItems
-    .map((el) => el.Items)
-    .flat()
-    .sort((a, b) => b.order_count - a.order_count);
-
-  const [items, setItems] = useState<IItem[]>([]);
-
-  useEffect(() => {
-    setItems(allItems.slice(0, 6));
-  }, []);
-
-  const otherCategory = (cat: number) => {
-    if (cat !== 99) {
-      setItems(allItems.filter((el) => el.category_id === cat).slice(0, 6));
-    } else {
-      setItems(allItems.slice(0, 6));
-    }
-  };
-
   return (
-    <div className="flex flex-col py-10 px-4">
-      <span className="text-2xl mb-4">Хиты продаж</span>
-      <div className="category-box flex border-b border-gray-400 justify-between overflow-y-auto mb-4">
-        <div className="flex gap-4 me-4">
-          <CatButton
-            title={'Все категории'}
-            autofocus={true}
-            otherCategory={otherCategory}
-            catNumber={99}
-          />
-          {aboutItems &&
-            aboutItems
-              .slice(0, 6)
-              .map((el) => (
-                <CatButton
-                  title={el.title}
-                  autofocus={false}
-                  key={el.id}
-                  otherCategory={otherCategory}
-                  catNumber={el.id}
-                />
-              ))}
-        </div>
-        <button className="text-sm py-4 border-b border-white text-red-500 focus:outline-none whitespace-nowrap">
-          Все хиты
-        </button>
-      </div>
-      <div className="card-box flex gap-2">
-        {items && items.map((el) => <ItemCard item={el} />)}
-      </div>
+    <div className="flex flex-col py-10 px-4 gap-44">
+      <FilteredItemsBox
+        title="Хиты продаж"
+        btnName="Все Хиты"
+        sortBy={'order_count'}
+        catNumber={99}
+      />
+
+      <FilteredItemsBox
+        title="Новинки"
+        btnName="Все новинки"
+        sortBy={'createdAt'}
+        catNumber={55}
+      />
     </div>
   );
 };
