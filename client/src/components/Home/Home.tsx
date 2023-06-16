a2871463dd34717736af10c726702e503ad6aeea
Egor Krasilnikov <krasilnikov19@gmail.com>
2023-06-16 09:40:13 +0300

    seeders update

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -7,36 +7,23 @@ import PacmanLoader from 'react-spinners/PacmanLoader';
 import { CatButton } from './CatButton';
 
 export const Home = () => {
-  const loading = useSelector((state: RootState) => state.loaderSlice.load);
-
   const categories = useSelector((state: RootState) => state.itemsSlice.items);
 
   return (
-    <>
-      {!loading && (
-        <div className="flex flex-col py-10 px-4">
-          <span className="text-2xl mb-4">Хиты продаж</span>
-          <div className="flex border-b border-gray-400 justify-between overflow-y-auto scroll-type">
-            <div className="flex gap-4 me-4">
-              <CatButton title={'Все категории'} autofocus={true} />
-              {categories &&
-                categories
-                  .slice(0, 6)
-                  .map((el) => (
-                    <CatButton title={el.title} autofocus={false} />
-                  ))}
-            </div>
-            <button className="text-sm py-4 border-b border-white text-red-500 focus:outline-none whitespace-nowrap">
-              Все хиты
-            </button>
-          </div>
+    <div className="flex flex-col py-10 px-4">
+      <span className="text-2xl mb-4">Хиты продаж</span>
+      <div className="flex border-b border-gray-400 justify-between overflow-y-auto scroll-type">
+        <div className="flex gap-4 me-4">
+          <CatButton title={'Все категории'} autofocus={true} />
+          {categories &&
+            categories
+              .slice(0, 6)
+              .map((el) => <CatButton title={el.title} autofocus={false} />)}
         </div>
-      )}
-      {loading && (
-        <Modal>
-          <PacmanLoader color="black" loading={loading} size={120} />
-        </Modal>
-      )}
-    </>
+        <button className="text-sm py-4 border-b border-white text-red-500 focus:outline-none whitespace-nowrap">
+          Все хиты
+        </button>
+      </div>
+    </div>
   );
 };
