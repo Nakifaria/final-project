b679dced3fc297728b5609a2839338e7c64a49d2
AnastasiaS <nakifaria@list.ru>
2023-06-17 20:28:08 +0300

    category img started

--- client/src/components/Catalog/Catalog.tsx
+++ client/src/components/Catalog/Catalog.tsx
@@ -39,7 +39,7 @@ export const Catalog = () => {
             onClick={() => {
               navigate("/configurator");
             }}
-            className=" mt-10 bg-blue-500 text-white p-6 rounded text-2xl font-bold overflow-hidden"
+            className="mt-10 bg-gray-800 hover:bg-gray-900 text-white p-6 rounded text-2xl font-bold overflow-hidden"
           >
             Собрать ПК
           </button>
@@ -54,20 +54,7 @@ export const Catalog = () => {
                 className="p-10 flex flex-col items-center text-center group md:lg:xl:border-r md:lg:xl:border-b hover:bg-slate-50 cursor-pointer"
               >
                 <span className="p-5 rounded-full bg-red-500 text-white shadow-lg shadow-red-200">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-10 w-10"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth="1.5"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
-                    />
-                  </svg>
+                 <img className="w-14" src="block.png"/>
                 </span>
                 <p className="text-xl font-medium text-slate-700 mt-3">
                   {el.title}
--- client/src/components/CategoryCatalog/CategoryCatalog.tsx
+++ client/src/components/CategoryCatalog/CategoryCatalog.tsx
@@ -103,7 +103,7 @@ export const CategoryCatalog = () => {
             onChange={changeHandler}
             value={priceData?.low}
             name="low"
-            type="text"
+            type="number"
             className="border rounded border-black"
           />
           до
@@ -111,7 +111,7 @@ export const CategoryCatalog = () => {
             onChange={changeHandler}
             value={priceData?.high}
             name="high"
-            type="text"
+            type="number"
             className="border rounded border-black"/>
 <button onClick={submitHandler} type="button" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
     Применить
