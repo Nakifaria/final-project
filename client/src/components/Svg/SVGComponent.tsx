e7f1647b30ed27cc7311ba5325c87a69bc1ebe86
Egor Krasilnikov <krasilnikov19@gmail.com>
2023-06-17 23:44:01 +0300

    some fixes

--- client/src/components/Svg/SVGComponent.tsx
+++ client/src/components/Svg/SVGComponent.tsx
@@ -119,16 +119,16 @@ export const SVGComponent: FC<ISvgProps> = ({ svgName, cartLength }) => {
         <svg
           fill="none"
           stroke="currentColor"
-          strokeWidth="1.5"
+          stroke-width="1.5"
           viewBox="0 0 24 24"
           xmlns="http://www.w3.org/2000/svg"
           aria-hidden="true"
           className="w-6"
         >
           <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M16.5 8.25V6a2.25 2.25 0 00-2.25-2.25H6A2.25 2.25 0 003.75 6v8.25A2.25 2.25 0 006 16.5h2.25m8.25-8.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-7.5A2.25 2.25 0 018.25 18v-1.5m8.25-8.25h-6a2.25 2.25 0 00-2.25 2.25v6"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75"
           ></path>
         </svg>
       );
